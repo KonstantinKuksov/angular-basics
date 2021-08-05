d4ba6c5fc9bce1c2c8d07ab0814210f2ecbb2be4
KonstantinKuksov <kkuksov@gmail.com>
2021-08-05 15:56:18 +0300

    learning forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,24 @@
-import { Component } from '@angular/core';
-import {AppCounterService} from "./services/app-counter.service";
-import {LocalCounterService} from "./services/local-counter.service";
-
-export interface Post {
-  title: string;
-  text: string
-}
+import {Component, OnInit} from '@angular/core'
+import {FormControl, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
-  providers: [LocalCounterService]
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
- constructor(public appCounterService: AppCounterService, public localCounterService: LocalCounterService) {
- }
+export class AppComponent implements OnInit {
+  form: FormGroup = new FormGroup({});
+
+  ngOnInit() {
+    this.form = new FormGroup({
+      email: new FormControl(''),
+      password: new FormControl(null),
+    });
+  }
+
+  submit() {
+    console.log('Form submitted: ', this.form);
+    console.log('Form data: ', {...this.form.value})
+  }
 }
+
